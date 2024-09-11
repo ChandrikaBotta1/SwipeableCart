4e7b583169f074d2ad6c66d32d1b30a41e161fc8
ChandrikaBotta1 <chandrikab@scaleorange.com>
2024-09-11 22:16:00 +0530

    Updates

---
+++ precache-manifest.fe093f96026f611a8b19e939bb16ca59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c708b18a88e85500ca9a771ac9cb823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd5298f54e6b1ed9377b",
+    "url": "/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "05a3cbfd2d362d9de2bf",
+    "url": "/static/js/2.63bded91.chunk.js"
+  },
+  {
+    "revision": "92b9e15012be958b3c7e4aaf01f55c0e",
+    "url": "/static/js/2.63bded91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5298f54e6b1ed9377b",
+    "url": "/static/js/main.51903c30.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
